05481122a34082398c3c0ab6e827ca9fc21e8b33
ToyamaKai <105268553+ToyamaKai@users.noreply.github.com>
2024-09-30 18:54:03 +0900

    WhiteBless.csにコメントの追記

--- Assets/Scripts/WhiteBless.cs
+++ Assets/Scripts/WhiteBless.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class WhiteBless : MonoBehaviour
 {
+    //パーティクルのアタッチを行う
     [SerializeField]
-    private ParticleSystem m_whiteBless;
-
+    private ParticleSystem m_whiteBless
+    
     private void Update()
     {
+        //マウスの左クリック時にパーティクルの発生を行う
         if(Input.GetMouseButtonDown(0))
         {
             m_whiteBless.Play();
